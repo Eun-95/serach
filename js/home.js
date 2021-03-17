c530ddc92ae538fb40abf28149f664bd7105eb17
eun taek <41125006+gongdae95@users.noreply.github.com>
2021-03-18 00:50:31 +0900

    upload
    upload

---
+++ js/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $("#m_nav_btn").click(function () {
+        $('#m_nav_wrap').fadeIn(200);
+    });
+
+    $("#m_nav_btn_close").click(function () {
+        $('#m_nav_wrap').fadeOut(200);
+    });
+
+});
